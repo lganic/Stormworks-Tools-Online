c14ffb71f1997b4582e95415541c1977a217a1d2
lganic <55366941+lganic@users.noreply.github.com>
2026-09-16 13:58:17 -0400

    Added block choice implementation to ellipse generator

--- tools/circle/js/app.js
+++ tools/circle/js/app.js
@@ -1,5 +1,8 @@
 'use strict';
 
+// Lookup util
+const $ = id => document.getElementById(id)
+
 // Fix zoom buttons propagating their events to the root plot.
 const zoomControls = document.querySelector(".zoom");
 
@@ -111,37 +114,41 @@ function generateEllipse(width, height, weight = 1, margin = .2) {
 
         const stop = a * a / Math.hypot(a, b) + margin
         const points = []
-        const lengths = [1, 1, 2, 4];
+        const lengths = [1]; // Base block
+
+        if ($('wedge1').checked) lengths.push(1); // Add 1 x 1 wedge
+        if ($('wedge2').checked) lengths.push(2); // Add 1 x 2 wedge
+        if ($('wedge3').checked) lengths.push(3); // Add 1 x 3 wedge
+        if ($('wedge4').checked) lengths.push(4); // Add 1 x 4 wedge
 
         // Update the current position based on the type of wedge. 0 = block, 1 = 1x1 wedge, 2 = 1x2 wedge, 3 = 1x3 wedge.
         function update(p, wedge_type) {
-            let dx;
 
-            if (wedge_type === 0 || wedge_type === 1)
-                dx = 1;
-            else if (wedge_type === 2)
-                dx = 2;
-            else
-                dx = 4;
+            let dx = lengths[wedge_type];
+            let dy = -1;
 
-            let dy = wedge_type === 0 ? 0 : -1;
+            if (wedge_type === 0) dy = 0; // Is block. Doesnt go down.
 
             return [p[0] + dx, p[1] + dy];
         }
 
         function best(p) {
-            const e = lengths.map((len, i) => (p[0] <= a - len ? ellipseError(p, len, i === 0 ? 0 : 1, a, b) : 99999) * (i === 0 ? weight : 1) / len);
+            const e = [];
 
-            if (e[0] < Math.min(e[1], e[2], e[3])) {
-                return 0;
+            for (let block_type = 0; block_type < lengths.length; block_type ++){
+
+                let error = 99999;
+
+                if (p[0] <= a - lengths[block_type]) error = ellipseError(p, lengths[block_type], block_type === 0 ? 0 : 1, a, b);
+
+                error *= (block_type === 0 ? weight : 1);
+
+                error /= lengths[block_type];
+
+                e.push(error);
             }
-            if (e[1] < Math.min(e[2], e[3])) {
-                return 1;
-            }
-            if (e[2] < e[3]) {
-                return 2;
-            }
-            return 3;
+
+            return e.indexOf(Math.min(...e)); // Return lowest error solution.
         }
 
         let p = [initial, b];
@@ -182,9 +189,6 @@ if (typeof module !== 'undefined') module.exports = { generate, integratedError,
 
 
 if (typeof document !== 'undefined') {
-
-    // Lookup util
-    const $ = id => document.getElementById(id)
 
     // Get elements from page.
     const canvas = $('canvas')
